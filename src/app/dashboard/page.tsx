6fb9a2a4da8218a30b9bc9f4ab1a479d2e86c7fb
gopikant21 <gopikant5jan@gmail.com>
2024-11-08 04:03:16 +0530

    dashboard started

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {DataTableDemo} from '@/components/ui/history';
 
 const DashboardPage: React.FC = () => {
     return (
         <div>
-            
+            <DataTableDemo/>
         </div>
     );
 };
