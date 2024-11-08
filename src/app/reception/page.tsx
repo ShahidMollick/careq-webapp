b86710b675bb7a3b436091097ab31497558e18fc
gopikant21 <gopikant5jan@gmail.com>
2024-11-09 02:26:09 +0530

    chcdhsgh

--- src/app/reception/page.tsx
+++ src/app/reception/page.tsx
@@ -3,6 +3,7 @@ import PrescriptionStatusBox from '@/components/ui/prescription-status-box';
 import React from 'react';
 
 const ReceptionistPage: React.FC = () => {
+   
     return (
         <div className='grid grid-cols-3 gap-4'>
             <AppointmentBox />
--- src/components/ui/appointment-box.tsx
+++ src/components/ui/appointment-box.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import {Button} from './button';
 import Image from 'next/image';
+import data from "../../app/appointment/appointments.json";
 
 const AppointmentBox: React.FC = () => {
+
+     // Get the first ticket number (from the 'serving' array)
+  const firstTicketNumber =
+  data.serving.length > 0 ? data.serving[0].ticketNumber : null;
+
+// Get the last ticket number (from the 'completed' array)
+const lastTicketNumber =
+  data.completed.length > 0
+    ? data.completed[data.completed.length - 1].ticketNumber
+    : null;
+
     return (
         <div className='flex flex-col gap-4 width-full max-w-lg'>
             <div className='flex flex-row justify-between items-center'>
                 <div className='flex flex-col'>
-                    <div className='text-md font-bold'>Prescription Status</div>
-                    <div className='text-sm text-gray-500'>Check the status of prescription</div>
+                    <div className='text-md font-bold'>Appointments</div>
+                    <div className='text-sm text-gray-500'>Check Appointments</div>
                 </div>
                 <Button className=' md text-black border border-solid rounded-lg bg-white border-slate-500/[0.37] ...'>View Complete List</Button>
             </div>
-            <div className='flex flex-row gap-4 border-b pb-4 border-slate-500/[0.37] ...'>
-                <Button  className='sm rounded-full bg-white border-solid border-slate-500/[0.37] ... text-black '>All</Button>
-                <Button  className='sm rounded-full bg-white border-solid border-slate-500/[0.37] ... text-black '>Pending</Button>
-                <Button  className='sm rounded-full bg-white border-solid border-slate-500/[0.37] ... text-black '>Uploaded</Button>
+            <div className="flex flex-row gap-2 w-full">
+              <div className="md text-sm w-full text-black text-center font-bold p-2 border border-solid rounded-lg border-slate-500/[0.37] ...">
+                Current Queue : {firstTicketNumber}
+              </div>
+              <div className="md text-sm w-full text-black text-center font-bold p-2 border border-solid rounded-lg border-slate-500/[0.37] ...">
+                Total Patients : {lastTicketNumber}
+              </div>
             </div>
             <div className='flex flex-col justify-between width-full'>
                 <div className='flex flex-row gap-4 items-center justify-between width-full py-2'>
