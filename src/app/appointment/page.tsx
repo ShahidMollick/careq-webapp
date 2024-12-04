f38c6ece93fdb699d0cbe52486c7ea2d5124361b
HariomArka <swapanghosh.8145@gmail.com>
2024-12-04 19:31:13 +0530

    Update page.tsx

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -194,7 +194,7 @@ const AppointmentPage: React.FC = () => {
 
             <Textarea
               placeholder="| Start typing prescription details here......"
-              className="overflow-auto h-full"
+              className="overflow-auto h-[80%]"
             />
             <div className="flex flex-row justify-between">
               <div className="w-30 h-20 p-1"><Image src="/clinic-sign.png" alt="sign" width={50} height={20} layout="responsive" /></div>
