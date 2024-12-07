f15d70498789f854d747ddfc8348cea1ebd2d933
HariomArka <swapanghosh.8145@gmail.com>
2024-12-08 00:41:52 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,53 +8,17 @@ import MetricBox from '@/components/ui/metric-box';
 import MetricBoxCustom from '@/components/ui/metric-box-custom';
 import { Component } from '@/components/ui/graph-cards';
 
-// import { TrendingUp } from "lucide-react"
-// import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
-
-// import {
-//     Card,
-//     CardContent,
-//     CardDescription,
-//     CardFooter,
-//     CardHeader,
-//     CardTitle,
-// } from "@/components/ui/card"
-// import {
-//     ChartConfig,
-//     ChartContainer,
-//     ChartTooltip,
-//     ChartTooltipContent,
-// } from "@/components/ui/chart"
-// import { Component } from '@/components/ui/graph-cards';
-// const chartData = [
-//     { month: "January", desktop: 186 },
-//     { month: "February", desktop: 305 },
-//     { month: "March", desktop: 237 },
-//     { month: "April", desktop: 73 },
-//     { month: "May", desktop: 209 },
-//     { month: "June", desktop: 214 },
-// ]
-
-// const chartConfig = {
-//     desktop: {
-//         label: "Desktop",
-//         color: "hsl(var(--chart-1))",
-//     },
-// } satisfies ChartConfig
-
 const DashboardPage: React.FC = () => {
     return (
         <div className='bg-gray-500/[.004] ...  rounded-md'>
-            {/* <div className='text-md font-bold'>Dashboard</div> */}
 
             {/* Need To be copied in every page */}
-
-            <div className="nav-bar item-center">
-                <div className="text-xl font-bold">Dashboard</div>
-                <div className="right">
+            <div className="nav-bar  w-[100%] flex item-center justify-between p-3">
+                <div className="font-bold text-[20px]">Dashboard</div>
+                <div className="right flex items-center">
                     <div className="clinic-option">
                         <ComboboxDemo />
-                        <Image src="/bell.svg" alt="bell" width={30} height={30} />
+                        <Image src="/bell.svg" alt="bell" width={20} height={20} />
                     </div>
                     <div className="doctor-profile">
                         <Image src="/doctor.svg" alt="doctor" width={40} height={40} />
@@ -67,47 +31,47 @@ const DashboardPage: React.FC = () => {
                 </div>
             </div>
 
-            {/* <DataTableDemo/> */}
 
-            <div className="current-consulting h-[80px] w-[100%] flex bg-[#e7f5f0] items-center gap-[22px] px-6 justify-between">
-                <div className='flex'>
-                    <Image src="/user.svg" alt="user" width={30} height={30} />
+            <div className="current-consulting h-[50px] w-[100%] flex bg-[#e7f5f0] items-center gap-[22px] px-6 justify-between text-[12px] py-4">
+                <div className='flex items-center gap-2'>
+                    <Image src="/user.svg" alt="user" width={20} height={20} />
                     <div className='text-[#164772]'>
                         Your consultation queue is now live! Patient #3 is waiting
                     </div>
                 </div>
-                <Button className="p-5 bg-green-600 text-[15px]">
-                    Continue Consultation
-                </Button>
+                <div className='flex items-center'>
+                    <Button variant="default">
+                        Continue Consultation
+                    </Button>
+                    <img className='w-[30px]' src="/plus.svg" alt="" />
+                </div>
             </div>
 
-            <div className="numbercards flex mt-[20px]">
+            <div className="numbercards flex mt-[20px] justify-around">
                 <MetricBoxCustom number={65} heading='Total Appointments'></MetricBoxCustom>
-
                 <MetricBoxCustom number={40} heading='Total Patient Waiting'></MetricBoxCustom>
-
                 <MetricBoxCustom number={25} heading='Total Patients Consulted'></MetricBoxCustom>
 
-                <div className="flex flex-row justify-between  border-1  border-radius: 13.391px bg-white shadow-lg border border-slate-200 rounded-lg p-3 w-[23%]  h-[120px] ml-[1.3rem]"
+                <div className="flex flex-row justify-between  border-1  border-radius: 13.391px bg-white shadow-lg border border-slate-200 rounded-lg p-3 w-[23%]  ml-[1.3rem]"
                     style={{
                         boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.05)",
                     }}
                 >
                     <div>
                         <div>
-                            <div className="font-bold">Total Revenue</div>
-                            <div className="text-green-600 font-bold">Today</div>
+                            <div className="font-bold text-[12px]">Total Revenue</div>
+                            <div className="text-green-600 text-xs font-bold">Today</div>
                         </div>
                         <div className="text-sm text-gray-500">Set the dimensions for the layer.</div>
                     </div>
-                    <div className="bg-primary-accent text-primary text-3xl font-bold p-2 item-center h-[50px] rounded-md">$3,200</div>
+                    <div className="bg-primary-accent text-primary text-xl font-bold p-2 item-center h-max rounded-md">$3,200</div>
                 </div>
             </div>
 
-                    <div className='ml-[20px] font-bold text-[20px]'>Overview</div>
+            <div className='ml-[20px] mt-[20px] font-bold text-[15px]'>Overview</div>
             <div className='flex mt-[10px]  justify-around'>
-                <Component />
-                <Component />
+                <Component/>
+                <Component/>
             </div>
 
         </div>
