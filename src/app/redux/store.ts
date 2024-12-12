c46098a22038c33cd9936f78aca19fb81638fcf2
gopikant21 <gopikant5jan@gmail.com>
2024-12-12 23:04:18 +0530

    redux toolkit installed

---
+++ src/app/redux/store.ts
@@ -0,0 +1,14 @@
+// redux/store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import appointmentReducer from './appointmentSlice';
+
+const store = configureStore({
+  reducer: {
+    appointments: appointmentReducer,
+  },
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
